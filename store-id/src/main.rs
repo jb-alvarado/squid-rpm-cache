e234aca31fe4fad51b392e537b79b34780fc9df4
Wade Berrier <wberrier@gmail.com>
2020-10-09 15:17:59 -0600

    squid rpm cache: exercise to implement in rust

---
+++ store-id/src/main.rs
@@ -0,0 +1,27 @@
+
+use text_io::read; // For easy stdin input
+use regex::Regex;
+
+fn main() {
+    // TODO: update urls to handle the repodata metadata
+    let url_regex = Regex::new(r"[^/]+\.rpm").unwrap();
+
+    loop {
+        let line: String = read!("{}\n");
+        let parts: Vec<&str> = line.split(" ").collect();
+
+        if parts.len() > 0 {
+            let url = parts[0];
+            let mut id = url;
+
+            // If it matches, only use the base rpm name instead of the full url
+            if let Some(captures) = url_regex.captures(url) {
+                id = captures.get(0).unwrap().as_str();
+            }
+
+            println!("OK store-id={}", id);
+        } else {
+            eprintln!("Unknown input: {}", line);
+        }
+    }
+}
